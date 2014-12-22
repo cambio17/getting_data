1d9b1a5a6037677d8ffa53bd669a44da2b74a2f7
cambio17 <cambio17@yahoo.com>
2014-12-22 02:10:13 +0200

    Course project files second commit

---
+++ getData.R
@@ -0,0 +1,20 @@
+getData <- function(directory, keyword) {
+    
+    setwd(directory)
+    
+    subject.file <- paste("subject_", keyword, ".txt", sep="")
+    y.file <- paste("y_", keyword, ".txt", sep="")
+    x.file <- paste("X_", keyword, ".txt", sep="")
+    
+    s <- read.table(subject.file)
+    y <- read.table(y.file)
+    x <- read.table(x.file)
+    
+    result <- cbind(s, y)
+    names(result)[1] <- "subject"
+    names(result)[2] <- "activity"
+    result <- cbind(result, x)
+    
+    message(paste("... got the", keyword, "dataset"))
+    return(result)
+}
